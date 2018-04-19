c30f4a439a4d5628b440ed5c106379aa0fa339d9
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-19 19:06:42 +0530

    Update CallbackController.php

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -415,8 +415,12 @@ class CallbackController extends Controller
         {
             $orderDetails = $order[0]; // Setting up the order details fetched
             $orderObj                     = pluginApp(stdClass::class);
+	$authHelper = pluginApp(AuthHelper::class);
+        $authHelper->processUnguarded(
+                function () use ($payment, $orderId) {
 		$order_object = $this->orderRepository->findOrderById($orderId);
 		$this->getLogger(__METHOD__)->error('callbackscript orderobject',  $order_obj);
+		});
             $orderObj->tid                = $this->aryCaptureParams['shop_tid'];
             $orderObj->order_total_amount = $orderDetails->amount;
             // Collect paid amount information from the novalnet_callback_history
