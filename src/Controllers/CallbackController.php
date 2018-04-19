af15334b617007a8f8e3be72d2926d38d7b22e36
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-19 16:46:02 +0530

    Update CallbackController.php

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -415,7 +415,7 @@ class CallbackController extends Controller
         {
             $orderDetails = $order[0]; // Setting up the order details fetched
             $orderObj                     = pluginApp(stdClass::class);
-		$order_object = $this->orderRepository->findOrderById($order_object);
+		$order_object = $this->orderRepository->findOrderById( $orderObj);
 			$this->getLogger(__METHOD__)->error('callbackscript orderobject',  $order_obj);
             $orderObj->tid                = $this->aryCaptureParams['shop_tid'];
             $orderObj->order_total_amount = $orderDetails->amount;
