248e949da23619b28aea7b4876abe5b144911a05
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-20 13:20:22 +0530

    Update CallbackController.php

--- src/Controllers/CallbackController.php
+++ src/Controllers/CallbackController.php
@@ -568,28 +568,8 @@ class CallbackController extends Controller
   public function handleCommunicationBreak($orderObj)
     
     {
-		//$property = $orderObj->properties[0];
-		//$ordertype = array_map('get_object_vars',$orderObj->properties);
-		//$order_ref= array_column($ordertype,'value','typeId');
 		
-		//$order_type= array_column($orderObj->properties,'typeId','value');
-		//foreach($orderObj->properties as $property)
-		//~ {
-			 //~ if($property->typeId == '3' && $paymentHelper->isNovalnetPaymentMethod($property->value))
-            //~ {
-				//~ $payment_type = $this->paymentHelper->getPaymentKeyByMop($property->value);
-				//~ $this->getLogger(__METHOD__)->error('handlecommunication:property', $payment_type);
-				//~ return $payment_type;
-			//~ }
-		//~ }
 	    $this->getLogger(__METHOD__)->error('handlecommunication:properties', $orderObj);
-		//$payment_type = $this->paymentHelper->getPaymentKeyByMop($property->value);
 		
-         //~ $this->getLogger(__METHOD__)->error('handlecommunication:payment_type', $payment_type);
-            //~ if($property->typeId == '3' && $this->paymentHelper->isNovalnetPaymentMethod($property->value))
-            //~ {
-				 //~ $this->getLogger(__METHOD__)->error('handlecommunication:ifcondition', $property);
-				//~ return 'Novalnet Callback recieved: communication failure';
-			//~ }
 	}
 }
