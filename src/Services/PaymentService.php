b08f3d15505f5ff5017316d189ceeee7b2831a03
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-23 18:22:32 +0530

    Add files via upload

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -601,7 +601,7 @@ class PaymentService
     */
     public function getRedirectPaymentUrl()
     {
-        return $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl . '/payment/novalnet/redirectPayment';
+        return $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl . '/payment/novalnet/onlineBank';
     }
     
     
