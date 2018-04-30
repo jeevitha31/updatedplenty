d3dc0d9d5478d785e5fbf278a701ac23b1adbc1d
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-30 17:54:32 +0530

    Update PaymentService.php

--- src/Services/PaymentService.php
+++ src/Services/PaymentService.php
@@ -609,7 +609,7 @@ class PaymentService
     */
     public function getRedirectPaymentUrl()
     {
-        return $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl . '/payment/novalnet/onlineBank';
+        return $this->webstoreHelper->getCurrentWebstoreConfiguration()->domainSsl . '/payment/novalnet/redirectPayment';
     }
     
     
